0f2c3bfa709f261d61f589f5b18e512b81799375
LeonardoDiPrimo <roberto.diprimo@davinci.edu.ar>
2021-04-12 18:37:51 -0300

    Modifico el header

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     <link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
     <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
     <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="css/styles.css" rel="stylesheet" />
+    <link href="css/estilos.css" rel="stylesheet" />
 </head>
 
 <body id="page-top">
@@ -41,9 +41,11 @@
     <!-- Masthead-->
     <header class="masthead">
         <div class="container">
+            <!-- 
             <div class="masthead-subheading">Welcome To Our Studio!</div>
             <div class="masthead-heading text-uppercase">It's Nice To Meet You</div>
-            <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
+            -->
+            <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Productos Destacados</a>
         </div>
     </header>
     <!-- Services-->
