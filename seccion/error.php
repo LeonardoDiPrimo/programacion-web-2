f448d3ac727f1ed58256bdacbe6192ff7ccfede4
LeonardoDiPrimo <roberto.diprimo@davinci.edu.ar>
2021-04-20 00:46:01 -0300

    Agrego secciones para tener mas ordenado el codigo

---
+++ seccion/error.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <h1>Error <small>Pagina no encontrada</small> </h1>
+        </div>
+    </div>
+</div>
+
+<?php require ("seccion/home.php"); ?>
