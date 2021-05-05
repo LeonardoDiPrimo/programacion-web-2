430151671df8ddcf6f28849f2fce48ca3391c0aa
LeonardoDiPrimo <roberto.diprimo@davinci.edu.ar>
2021-05-05 13:19:51 -0300

    Ordenar codigo

--- include/header.php
+++ include/header.php
@@ -1,50 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <title>Agency - Start Bootstrap Theme</title>
-    <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
-    <!-- Font Awesome icons (free version)-->
-    <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
-    <!-- Google fonts-->
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
-    <link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
-    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
-    <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="css/estilos.css" rel="stylesheet" />
-</head>
-
-<body id="page-top">
-    <!-- Navigation-->
-    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
-        <div class="container">
-            <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="assets/img/navbar-logo.svg" alt="" /></a>
-            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                Menu
-                <i class="fas fa-bars ml-1"></i>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav text-uppercase ml-auto">
-                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?seccion=home">Home</a></li>
-                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?seccion=products">Productos</a></li>
-                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?seccion=services">Services</a></li>
-                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?seccion=about">About</a></li>
-                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?seccion=team">Team</a></li>
-                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?seccion=contact">Contacto</a></li>
-                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="admin/seccion/login.php">login</a></li>
-                </ul>
-            </div>
+<!-- Navigation-->
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+    <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="assets/img/navbar-logo.svg" alt="" /></a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            Menu
+            <i class="fas fa-bars ml-1"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav text-uppercase ml-auto">
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?seccion=home">Home</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?seccion=products">Productos</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?seccion=services">Services</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?seccion=about">About</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?seccion=team">Team</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?seccion=contact">Contacto</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="admin/seccion/login.php">login</a></li>
+            </ul>
         </div>
-    </nav>
-    <!-- Masthead-->
-    <header class="masthead">
-        <div class="container">
-            <!-- <div class="masthead-subheading">Welcome To Our Studio!</div>
+    </div>
+</nav>
+<!-- Masthead-->
+<header class="masthead">
+    <div class="container">
+        <!-- <div class="masthead-subheading">Welcome To Our Studio!</div>
             <div class="masthead-heading text-uppercase">It's Nice To Meet You</div>-->
-            <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="index.php?seccion=home">Productos Destacados</a>
-        </div>
-    </header>
+        <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="index.php?seccion=home">Productos Destacados</a>
+    </div>
+</header>
--- index.php
+++ index.php
@@ -1,5 +1,26 @@
- <?php
-    // El index carga la barra de navegacion
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Agency - Start Bootstrap Theme</title>
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/estilos.css" rel="stylesheet" />
+</head>
+
+<body id="page-top">
+    <?php
+    // Barra de navegación
     require_once("include/header.php");
 
     //Cargamos todos los php necesarios para generar los Json y que la pagina funcione
@@ -32,20 +53,20 @@
         require("seccion/home.php");
     }
 
-    // El index carga el footer
+    // Footer
     require_once("include/footer.php");
     ?>
 
- <!-- Bootstrap core JS-->
- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
- <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
- <!-- Third party plugin JS-->
- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
- <!-- Contact form JS-->
- <script src="assets/mail/jqBootstrapValidation.js"></script>
- <script src="assets/mail/contact_me.js"></script>
- <!-- Core theme JS-->
- <script src="js/scripts.js"></script>
- </body>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Third party plugin JS-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+    <!-- Contact form JS-->
+    <script src="assets/mail/jqBootstrapValidation.js"></script>
+    <script src="assets/mail/contact_me.js"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+</body>
 
- </html>
+</html>
