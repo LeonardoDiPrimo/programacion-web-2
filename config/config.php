d398be596a084a151c8564c84e03b68569b7a693
chuk4 <kevin.asanchez@davinci.edu.ar>
2021-06-13 02:36:50 -0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,4 +1,4 @@
 <?php
-    define('DIR_BASE','C:/xampp/htdocs/programacion-web-2/');
+    define('DIR_BASE','/xampp/htdocs/programacion-web-2/');
     define('URL_BASE','http://localhost/programacion-web-2/');
 ?>
